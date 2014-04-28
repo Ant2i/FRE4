ffc7d287371986e5c9bdf3a1739c26e2806d0a09
Ant2i <antuan2k@gmail.com>
2014-04-28 20:09:18 +0400

    Change math headers.

---
+++ Dev/External/Math/MathGLM.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <glm/glm.hpp>
+
+typedef glm::vec4 Vector4f_t;
+typedef glm::quat Quaterion_t;
+typedef glm::mat4 Matrix4f_t;
+typedef glm::mat3 Matrix3f_t;
+
+float VectorDot(const Vector3f_t & v1, const Vector3f_t & v2)
+{
+	return 0.0f;
+}
+
+Vector3f_t VectorCross(const Vector3f_t & v1, const Vector3f_t & v2)
+{
+	//return 0.0f;
+}
--- Dev/Source/Engine/Include/Math.h
+++ Dev/Source/Engine/Include/Math.h
@@ -4,19 +4,8 @@ namespace FRE4
 {
 	namespace Math
 	{
-		typedef float Vector3f_t;
-		typedef float Quaterion_t;
-		typedef float Matrix4f_t;
-		typedef float Matrix3f_t;
-
-		float VectorDot(const Vector3f_t & v1, const Vector3f_t & v2)
-		{
-			return 0.0f;
-		}
-
-		Vector3f_t VectorCross(const Vector3f_t & v1, const Vector3f_t & v2)
-		{
-			//return 0.0f;
-		}
+		//#include <MathGLM.h>
+		//#include <MathEigen.h>
+		#include <cmath>
 	}
 }
