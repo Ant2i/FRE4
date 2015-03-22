24715cb589699f4f4727dbeb3b67d21ccfed09b7
ant2i <ant2i@ant2i-Aspire-3690>
2015-03-23 02:20:38 +0300

    X11

---
+++ Dev/Source/RenderGL/X11/OpenGLX11API.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include "GL/glx.h"
+#include "GL/glxext.h"
+
+#include "GLDecl.h"
+
+#define DECLARE_GL_ENTRYPOINTS(Type, Func) Type Func;
+ENUM_GL_ENTRYPOINTS_ALL(DECLARE_GL_ENTRYPOINTS);
+
+#include "OpenGL4API.h"
+
+namespace FRE
+{
+	struct OpenGLX11API : public OpenGL4API
+	{
+	};
+
+	typedef OpenGLX11API FOpenGL;
+}
