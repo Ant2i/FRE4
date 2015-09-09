3c1833008a5ae5470b679405aa9a910521ef743c
ant2i <ant2i@G550JK>
2015-09-10 02:34:31 +0300

    Add native platform project.

--- Dev/Source/Platform/Platform.h
+++ Dev/Source/Platform/Platform.h
@@ -1,28 +1,35 @@
 #pragma once
 
-#include "FStream.h"
+#include "FPlatform.h"
 
-class IFileHandle : public IStream
+#ifdef SYS_PLATFORM_EXPORTS
+#define SYSP_API API_EXPORT_C
+#else
+#define SYSP_API API_IMPORT_C
+#endif
+
+//#include "FStream.h"
+
+class IFileStream //: public IStream
 {
-	public:
+public:
 
 };
 
 class IPlatformFile
 {
 public:
-	virtual IFileHandle * OpenRead(const wchar_t * fileName) = 0;
-	virtual IFileHandle * OpenWrite(const wchar_t * fileName, bool append = false, bool allowRead = false) = 0;
-
+	virtual IFileStream * OpenRead(const wchar_t * fileName) = 0;
+	virtual IFileStream * OpenWrite(const wchar_t * fileName, bool append = false, bool allowRead = false) = 0;
 
 };
 
 class IPlatformLibrary
 {
 public:
-	virtual void * LoadLibrary( const wchar_t * fileName ) = 0;
-	virtual void FreeLibrary( void* handle ) = 0;
-	virtual void* ExportProc( void* handle, const wchar_t * procName) = 0;
+	virtual void * LoadLibrary(const wchar_t * fileName) = 0;
+	virtual void FreeLibrary(void* handle) = 0;
+	virtual void* ExportProc(void* handle, const wchar_t * procName) = 0;
 
 	virtual void PushFindLibraryDirectory(const wchar_t * directory) = 0;
 	virtual void PopFindLibraryDirectory() = 0;
@@ -30,6 +37,9 @@ public:
 	virtual const wchar_t * BaseDirectory() = 0;
 	virtual const wchar_t * ExecutableName(bool bRemoveExtension = true) = 0;
 
-	static const wchar_t * GetLibraryExtension() = 0;
+	virtual const wchar_t * GetLibraryExtension() = 0;
 };
+
+SYSP_API IPlatformFile & PlatformFile();
+SYSP_API IPlatformLibrary & PlatformLibrary();
 
