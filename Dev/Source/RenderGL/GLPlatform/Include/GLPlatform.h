535213b2aea7b9f6f946560d05b6c1e09860dbe9
unknown <Anischenko@Anischenko.TSDomain.ru>
2015-10-23 19:30:12 +0300

    Redesigned GLP interface

--- Dev/Source/RenderGL/GLPlatform/Include/GLPlatform.h
+++ Dev/Source/RenderGL/GLPlatform/Include/GLPlatform.h
@@ -14,8 +14,8 @@
 #define GLCP_API API_IMPORT_C
 #endif
 
-class GLPlatformContext;
-class GLPlatformRenderSurface;
+struct GLPlatformContext;
+struct GLPlatformRenderSurface;
 
 typedef GLPlatformContext * GLPlatformContextP;
 typedef GLPlatformRenderSurface * GLPlatformRenderSurfaceP;
@@ -23,6 +23,51 @@ typedef GLPlatformRenderSurface * GLPlatformRenderSurfaceP;
 typedef unsigned long long int GLSurfaceFormatH;
 
 //-----------------------------------------------------------------------------
+
+typedef void * GLPContext;
+typedef void * GLPSurface;
+typedef void * GLPConfig;
+typedef void * GLPNativeWindowType;
+
+struct PGLSurfaceDesc
+{
+	
+};
+
+struct PGLContextDesc
+{
+
+};
+
+struct PGLConfigDesc
+{
+	int RedSize;
+	int GreenSize;
+	int BlueSize;
+	int DepthSize;
+	int Stereo;
+};
+
+GLCP_API bool PGLInitialize(unsigned * oMajorVer, unsigned * oMinorVer, bool iDebugMode);
+GLCP_API void PGLTerminate();
+
+GLCP_API PGLConfigDesc PGLDefaultConfigDesc();
+GLCP_API GLPConfig PGLChooseConfig(const PGLConfigDesc * iDesc);
+GLCP_API bool PGLGetConfigDesc(GLPConfig iConfig, PGLConfigDesc * oDesc);
+
+GLCP_API GLPContext PGLContextCreate(GLPConfig iConfig, GLPContext iSharedContext = nullptr, const PGLContextDesc * iDesc = nullptr);
+GLCP_API void PGLContextDestroy(GLPContext iContext);
+
+GLCP_API GLPSurface PGLSurfaceCreate(GLPConfig iConfig, GLPNativeWindowType iWindow, const PGLSurfaceDesc * iDesc = nullptr);
+GLCP_API void PGLSurfaceDestroy(GLPSurface iSurface);
+
+GLCP_API bool PGLContextMakeCurrent(GLPContext iContext, GLPSurface iSurface = nullptr);
+GLCP_API bool PGLSwapBuffers(GLPSurface iSurface);
+
+GLCP_API GLPContext PGLGetCurrentContext();
+
+
+//------------------------------------------------------------------------------
 
 struct SurfaceDesc
 {
